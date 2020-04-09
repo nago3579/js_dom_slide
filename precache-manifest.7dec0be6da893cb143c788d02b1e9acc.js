9bb1211bf7ca91ee0e0805d98bd4ffbaaeec07f2
nadiv <nago3579@colorado.edu>
2020-04-09 13:29:00 -0600

    Updates

---
+++ precache-manifest.7dec0be6da893cb143c788d02b1e9acc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202b6372649ca796a84371bf299ccbb3",
+    "url": "/js_dom_slide/index.html"
+  },
+  {
+    "revision": "8207df2952a4cc1fc565",
+    "url": "/js_dom_slide/static/css/2.dd1fe7f4.chunk.css"
+  },
+  {
+    "revision": "71484cafc3d214ce2b83",
+    "url": "/js_dom_slide/static/css/main.2fe0686d.chunk.css"
+  },
+  {
+    "revision": "8207df2952a4cc1fc565",
+    "url": "/js_dom_slide/static/js/2.f0976b6e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/js_dom_slide/static/js/2.f0976b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71484cafc3d214ce2b83",
+    "url": "/js_dom_slide/static/js/main.a273cf55.chunk.js"
+  },
+  {
+    "revision": "548e148e6b50a4001ce8",
+    "url": "/js_dom_slide/static/js/runtime-main.a963a2af.js"
+  },
+  {
+    "revision": "84647fef4dc873574ad26ed7076fae8b",
+    "url": "/js_dom_slide/static/media/fall_1.84647fef.jpg"
+  },
+  {
+    "revision": "ed343198cee38f1c779c9af60e5cfed3",
+    "url": "/js_dom_slide/static/media/fall_2.ed343198.jpg"
+  },
+  {
+    "revision": "3a907ae4392cdd15b0edbe42cf33a376",
+    "url": "/js_dom_slide/static/media/fall_3.3a907ae4.jpg"
+  },
+  {
+    "revision": "9afd832b99a30a22ea2b0de037fc016a",
+    "url": "/js_dom_slide/static/media/spring_1.9afd832b.jpg"
+  },
+  {
+    "revision": "359830f409cff984bd20a5bbf2595ac4",
+    "url": "/js_dom_slide/static/media/spring_2.359830f4.jpg"
+  },
+  {
+    "revision": "7d4f60bf7af5731ecba6735303f3fb50",
+    "url": "/js_dom_slide/static/media/spring_3.7d4f60bf.jpg"
+  },
+  {
+    "revision": "f72f432d5677d3b512c212f4e6985fda",
+    "url": "/js_dom_slide/static/media/summer_1.f72f432d.jpg"
+  },
+  {
+    "revision": "55da6b52110e9bf8f0662c7f84e829a2",
+    "url": "/js_dom_slide/static/media/summer_2.55da6b52.jpg"
+  },
+  {
+    "revision": "3191189be39555cf719887b6af060697",
+    "url": "/js_dom_slide/static/media/summer_3.3191189b.jpg"
+  },
+  {
+    "revision": "d57b6e07f2900bbc6bac8b27960001af",
+    "url": "/js_dom_slide/static/media/winter_1.d57b6e07.jpeg"
+  },
+  {
+    "revision": "60fb7c8d1116a03453ff2d20aad36233",
+    "url": "/js_dom_slide/static/media/winter_2.60fb7c8d.jpg"
+  },
+  {
+    "revision": "a6dd05da63bd6bb7265122b3144ed87b",
+    "url": "/js_dom_slide/static/media/winter_3.a6dd05da.jpg"
+  }
+]);
